02256531dc7616ac316638f6a86edcb172d707fc
MCMIMI <webmaster@momsclub.org>
2023-11-18 12:41:30 -0500

    update auth routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
-use Illuminate\Support\Facades\Auth;
+//use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Auth\ConfirmPasswordController;
+use App\Http\Controllers\Auth\VerificationController;
 use App\Http\Controllers\BoardController;
 use App\Http\Controllers\ChapterController;
 use App\Http\Controllers\CoordinatorController;
@@ -23,9 +27,36 @@ use App\Http\Controllers\ReportController;
 |
 */
 
-Auth::routes();
 
-Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+// Login and Logout Routes...
+Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('login', [LoginController::class,'login']);
+Route::post('logout',  [LoginController::class,'logout'])->name('logout');
+
+// Registration Routes...
+Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('register', [RegisterController::class, 'register']);
+
+// Password Reset Routes...
+Route::get('password/reset', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
+Route::post('password/email', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+Route::get('password/reset/{token}', [ForgotPasswordController::class, 'showResetForm'])->name('password.reset');
+Route::post('password/reset', [ForgotPasswordController::class, 'reset'])->name('password.update');
+
+// Confirm Password
+Route::get('password/confirm', [ConfirmPasswordController::class, 'showConfirmForm'])->name('password.confirm');
+Route::post('password/confirm', [ConfirmPasswordController::class, 'confirm']);
+
+// Email Verification Routes...
+Route::get('email/verify', [VerificationController::class, 'show'])->name('verification.notice');
+Route::get('email/verify/{id}/{hash}', [VerificationController::class, 'verify'])->name('verification.verify');
+Route::get('email/resend',  [VerificationController::class, 'resend'])->name('verification.resend');
+
+
+
+//Auth::routes();
+
+//Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
 
 Route::middleware(['preventBackHistory'])->group(function () {
     Route::get('/home', [HomeController::class, 'index'])->name('home');
