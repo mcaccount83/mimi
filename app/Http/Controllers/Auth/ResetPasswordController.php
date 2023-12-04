973f88594be4b4d0933f19ac24e14cabbb121287
Shift <shift@laravelshift.com>
2023-12-04 15:32:58 +0000

    Apply Laravel coding style
    Shift automatically applies the Laravel coding style - which uses the PSR-12 coding style as a base with some minor additions.
    
    You may customize the code style applied by configuring [Pint](https://laravel.com/docs/pint), [PHP CS Fixer](https://github.com/FriendsOfPHP/PHP-CS-Fixer), or [PHP CodeSniffer](https://github.com/squizlabs/PHP_CodeSniffer) for your project root.
    
    For more information on customizing the code style applied by Shift, [watch this short video](https://laravelshift.com/videos/shift-code-style).

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -4,15 +4,12 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 use Illuminate\View\View;
-use Carbon\Carbon;
 
 class ResetPasswordController extends Controller
 {
@@ -22,7 +19,6 @@ class ResetPasswordController extends Controller
      * Display the password reset view.
      *
      * @param  string  $token
-     * @return \Illuminate\View\View
      */
     public function showResetForm($token): View
     {
@@ -31,27 +27,24 @@ class ResetPasswordController extends Controller
 
     /**
      * Reset the given user's password.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\RedirectResponse
      */
     protected function reset(Request $request): RedirectResponse
     {
         $request->validate($this->rules(), $this->validationErrorMessages());
 
         $updatePassword = DB::table('password_resets')
-                              ->where([
-                                'email' => $request->email,
-                                'token' => $request->token
-                              ])
-                              ->first();
+            ->where([
+                'email' => $request->email,
+                'token' => $request->token,
+            ])
+            ->first();
 
-        if (!$updatePassword) {
+        if (! $updatePassword) {
             return back()->withInput()->with('error', 'Invalid token!');
         }
 
         $user = User::where('email', $request->email)
-                    ->update(['password' => Hash::make($request->password)]);
+            ->update(['password' => Hash::make($request->password)]);
 
         DB::table('password_resets')->where(['email' => $request->email])->delete();
 
