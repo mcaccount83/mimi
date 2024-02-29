78313bd5d705e0f7b4f71dbaf070eb26e1b369b2
Shift <shift@laravelshift.com>
2024-02-29 20:23:14 +0000

    Apply Laravel coding style
    Shift automatically applies the Laravel coding style - which uses the PSR-12 coding style as a base with some minor additions.
    
    You may customize the code style applied by configuring [Pint](https://laravel.com/docs/pint), [PHP CS Fixer](https://github.com/FriendsOfPHP/PHP-CS-Fixer), or [PHP CodeSniffer](https://github.com/squizlabs/PHP_CodeSniffer) for your project root.
    
    For more information on customizing the code style applied by Shift, [watch this short video](https://laravelshift.com/videos/shift-code-style).

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -4,9 +4,10 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+
 class ForgotPasswordController extends Controller
 {
-     /*
+    /*
     | Password Reset Controlle
     */
     use SendsPasswordResetEmails;
