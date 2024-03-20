4324106b3cd39cc3215e00a789a60aee810627ec
mcaccount83 <130937484+mcaccount83@users.noreply.github.com>
2024-03-20 11:17:08 -0400

    spatie remove update

--- resources/views/boardlist/show.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <h1>{{ $post->title }}</h1>
-    <p>{{ $post->content }}</p>
-    <!-- Add more details like author, creation date, etc. -->
-    <a href="{{ route('boardlist.index') }}">Back to BoardList</a>
-@endsection
