b6f08e8b7bc090aa2582d4221564812afac4c152
mcaccount83 <130937484+mcaccount83@users.noreply.github.com>
2024-03-24 11:44:42 -0400

    Update PaymentsReRegChapterThankYou.php

--- app/Mail/PaymentsReRegChapterThankYou.php
+++ app/Mail/PaymentsReRegChapterThankYou.php
@@ -5,6 +5,7 @@ namespace App\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Carbon;
 
 class PaymentsReRegChapterThankYou extends Mailable
 {
@@ -20,7 +21,16 @@ class PaymentsReRegChapterThankYou extends Mailable
     public function __construct($mailData)
     {
         $this->mailData = $mailData;
-        $this->mailData['chapterDate'] = date('m-d-Y', strtotime($this->mailData['chapterDate']));
+        // $this->mailData['chapterDate'] = date('m-d-Y', strtotime($this->mailData['chapterDate']));
+
+        // Check if 'chapterDate' is set and valid
+        if (!isset($this->mailData['chapterDate']) || !strtotime($this->mailData['chapterDate'])) {
+            // If 'chapterDate' is not set or invalid, default it to today's date
+            $this->mailData['chapterDate'] = Carbon::today()->format('m-d-Y');
+        } elseif (strpos($this->mailData['chapterDate'], '-') !== false) {
+            // If 'chapterDate' contains '-', assume it's in 'yyyy-mm-dd' format and format it
+            $this->mailData['chapterDate'] = date('m-d-Y', strtotime($this->mailData['chapterDate']));
+        }
     }
 
     /**
