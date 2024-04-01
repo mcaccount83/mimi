f8a6745c476add320b5c28a3801055dbf0195cbc
Shift <shift@laravelshift.com>
2024-04-01 19:07:58 +0000

    Remove unnecessary `$model` property

--- database/factories/ChapterFactory.php
+++ database/factories/ChapterFactory.php
@@ -8,13 +8,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class ChapterFactory extends Factory
 {
     /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = Chapter::class;
-
-    /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
--- database/factories/CoordinatorFactory.php
+++ database/factories/CoordinatorFactory.php
@@ -8,13 +8,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class CoordinatorFactory extends Factory
 {
     /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = Coordinator::class;
-
-    /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
