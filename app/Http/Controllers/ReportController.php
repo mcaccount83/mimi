b11a7b5231acea23d648d3f9b48ed8f324b02055
MCMIMI <webmaster@momsclub.org>
2024-07-01 18:18:53 -0400

    report emails update

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -1660,10 +1660,16 @@ class ReportController extends Controller
             ->join('board_details', 'chapters.id', '=', 'board_details.chapter_id')
             ->whereIn('board_details.board_position_id', [1, 2, 3, 4, 5])
             ->where('chapters.conference', $corConfId)
-            ->where('chapters.new_board_submitted', '=', '0')
+            ->where(function($query) {
+                $query->where('chapters.new_board_submitted', '=', '0')
+                      ->orWhereNull('chapters.new_board_submitted');
+            })
             ->where('created_at', '<=', date('Y-06-30'))
             ->where('chapters.is_active', 1)
             ->get();
+
+            // Debugging: Log the chapters retrieved
+    Log::info('Chapters with Board Reports Due: ', $chapters->toArray());
 
         $cc_email = [];
         $chapterEmails = []; // Store email addresses for each chapter
@@ -1752,7 +1758,10 @@ class ReportController extends Controller
             ->join('board_details', 'chapters.id', '=', 'board_details.chapter_id')
             ->whereIn('board_details.board_position_id', [1, 2, 3, 4, 5])
             ->where('chapters.conference', $corConfId)
-            ->where('chapters.financial_report_received', '=', '0')
+            ->where(function($query) {
+                $query->where('chapters.financial_report_received', '=', '0')
+                      ->orWhereNull('chapters.financial_report_received');
+            })
             ->where('created_at', '<=', date('Y-06-30'))
             ->where('chapters.is_active', 1)
             ->get();
