e91b03df143d35c89e285b4ef664f85dde4a0e40
MCMIMI <webmaster@momsclub.org>
2024-08-14 20:12:03 -0400

    error page updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,17 +3,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
     <title>{{config('app.name')}}</title>
-
-    {{-- <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
 
     <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -23,7 +15,6 @@
   <link rel="stylesheet" href="{{ env('BASE_URL') }}coordinator_theme/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
   <!-- Theme style -->
   <link rel="stylesheet" href="{{ env('BASE_URL') }}coordinator_theme/dist/css/adminlte.min.css">
-
     <script>
         window.onload = function () {
             if (window.history && window.history.pushState) {
@@ -34,13 +25,10 @@
             }
         };
     </script>
-
 </head>
 <body>
     <div id="app">
-
-            @yield('content')
-
+    @yield('content')
     </div>
 
 <!-- jQuery -->
@@ -50,7 +38,6 @@
 <!-- AdminLTE App -->
 <script src="{{ env('BASE_URL') }}dist/js/adminlte.min.js"></script>
 </body>
-<!-- Scripts -->
-{{-- <script src="{{ asset('js/app.js') }}" defer></script> --}}
+
 @yield('customscript')
 </html>
