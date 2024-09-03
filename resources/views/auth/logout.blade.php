ed911b426248c7ed9880e808191017f04eb9a204
MCMIMI <webmaster@momsclub.org>
2024-09-03 18:08:06 -0400

    force logout update

---
+++ resources/views/auth/logout.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.public_theme')
+
+@section('content')
+<div class="row justify-content-center">
+
+    <body class="hold-transition login-page">
+        <div class="login-box">
+            <div class="login-logo">
+                <a href="{{ route('home') }}" class="band-link">
+                    <img src="{{ config('settings.base_url') }}theme/dist/img/logo.png" alt="MC" width="120">
+                </a>
+                    </div>
+                      <!-- /.login-logo -->
+                <div class="card">
+                    <div class="card-body login-card-body">
+
+                        <button onclick="forceLogout()">Logout</button>
+
+
+                    </form>
+
+
+                    </div>
+                    <!-- /.login-card-body -->
+                </div>
+            </div>
+        </div>
+    </body>
+@endsection
+@section('customscript')
+<script>
+
+function forceLogout() {
+    fetch('{{ route('logout') }}', {
+        method: 'POST',
+        headers: {
+            'X-CSRF-TOKEN': '{{ csrf_token() }}',
+            'Content-Type': 'application/json'
+        },
+        credentials: 'same-origin'
+    }).then(response => {
+        if (response.ok) {
+            window.location.href = '/'; // Redirect after logout
+        }
+    });
+}
+
+
+</script>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,9 @@ Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::get('/', [LoginController::class, 'showLoginForm'])->name('home');
 Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('login', [LoginController::class, 'login']);
-Route::post('logout', [LoginController::class, 'logout'])->name('logout');
+Route::get('logout', [LoginController::class, 'logout'])->name('logout');
+Route::post('logout', [LoginController::class, 'logout']);
+
 
 // Password Reset Routes...
 Route::get('password/request', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
