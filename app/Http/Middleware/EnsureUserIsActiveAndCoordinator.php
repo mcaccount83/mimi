2136cffbd4e435f0d21623b9e8be4db392ae045d
Shift <shift@laravelshift.com>
2024-12-11 18:52:04 +0000

    Remove redundant typing from DocBlocks

--- app/Http/Middleware/EnsureUserIsActiveAndCoordinator.php
+++ app/Http/Middleware/EnsureUserIsActiveAndCoordinator.php
@@ -11,8 +11,6 @@ class EnsureUserIsActiveAndCoordinator
 {
     /**
      * Handle an incoming request.
-     *
-     * @return mixed
      */
     public function handle(Request $request, Closure $next): Response
     {
--- app/Http/Requests/UpdatePasswordUserRequest.php
+++ app/Http/Requests/UpdatePasswordUserRequest.php
@@ -8,8 +8,6 @@ class UpdatePasswordUserRequest extends FormRequest
 {
     /**
      * Get the validation rules that apply to the request.
-     *
-     * @return array
      */
     public function rules(): array
     {
