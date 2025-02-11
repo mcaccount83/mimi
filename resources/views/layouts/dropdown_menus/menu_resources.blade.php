64bed15534c024baa31c77465d89bc98e69a0341
MCMIMI <webmaster@momsclub.org>
2025-02-11 17:38:04 -0500

    Update menu_resources.blade.php

--- resources/views/layouts/dropdown_menus/menu_resources.blade.php
+++ resources/views/layouts/dropdown_menus/menu_resources.blade.php
@@ -1,6 +1,6 @@
 <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
     <a class="dropdown-item" href="{{ route('admin.toolkit') }}">Coordinator Toolkit</a>
-    <a class="dropdown-item" href="{{ url(config('forum.frontend.router.prefix') . '/c/2-vollist') }}" target="_blank">Vollist - NEW!</a>
+    {{-- <a class="dropdown-item" href="{{ url(config('forum.frontend.router.prefix') . '/c/2-vollist') }}" target="_blank">Vollist - NEW!</a> --}}
     <a class="dropdown-item" href="{{ route('admin.resources') }}">Chapter Resources</a>
     @if ($assistConferenceCoordinatorCondition)
         <a class="dropdown-item" href="{{ route('admin.downloads') }}">Download Reports</a>
