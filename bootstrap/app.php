c2b29b9efceba723a6cd810a1dc0f29206dd6d44
Shift <shift@laravelshift.com>
2025-03-13 00:31:37 +0000

    Re-register exception handling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,8 @@ use App\Providers\AppServiceProvider;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withProviders()
@@ -21,5 +23,16 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->throttleApi();
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->reportable(function (Throwable $e) {
+            $context = [
+                'url' => request()->fullUrl(),
+                'method' => request()->method(),
+                'user' => Auth::id() ?? 'guest',
+                'input' => collect(request()->all())->except(['password'])->toArray(),
+                'referrer' => request()->headers->get('referer'),
+                'trace' => $e->getTraceAsString(),
+            ];
+
+            Log::error($e->getMessage(), $context);
+        });
     })->create();
