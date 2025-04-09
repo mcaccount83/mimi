49f675b82375f3e6b89270eb64159c1546e47bdc
Shift <shift@laravelshift.com>
2025-04-09 16:51:23 +0000

    Adopt Laravel type hints

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Middleware;
 
+use Symfony\Component\HttpFoundation\Response;
 use Closure;
 use Illuminate\Http\Request;
 
 class AdminMiddleware
 {
-    public function handle(Request $request, Closure $next)
+    public function handle(Request $request, Closure $next): Response
     {
         if (! $request->user() || $request->user()->is_admin != 1) {
             return redirect('/login')->with('error', 'You need admin privileges to access this page.');
