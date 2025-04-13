b01538ceedc6579033dfff34a8e97174c7d976c1
MCMIMI <webmaster@momsclub.org>
2025-04-13 15:37:14 -0400

    admin menu

--- resources/views/layouts/dropdown_menus/menu_admin.blade.php
+++ resources/views/layouts/dropdown_menus/menu_admin.blade.php
@@ -6,5 +6,6 @@
       <a class="dropdown-item" href="{{ route('admin.googledrive') }}">Google Drive Settings</a>
       <a class="dropdown-item" href="{{ url(config('queue-monitor.ui.route.prefix')) }}">Mail Queue</a>
       <a class="dropdown-item" href="{{ url(config('sentemails.routepath')) }}">Sent Mail Log</a>
+      <a class="dropdown-item" href="{{ route('payment-logs.index') }}">Payments Log</a>
       <a class="dropdown-item" href="{{ route('logs') }}">System Error Logs</a>
   </div>
--- resources/views/layouts/dropdown_menus/menu_reports_admin.blade.php
+++ resources/views/layouts/dropdown_menus/menu_reports_admin.blade.php
@@ -7,5 +7,6 @@
       <a class="dropdown-item" href="{{ route('adminreports.disbandedboard') }}">Disbanded Board Members</a>
       <a class="dropdown-item" href="{{ url(config('queue-monitor.ui.route.prefix')) }}">Mail Queue</a>
       <a class="dropdown-item" href="{{ url(config('sentemails.routepath')) }}">Sent Mail Log</a>
+      <a class="dropdown-item" href="{{ route('payment-logs.index') }}">Payments Log</a>
       <a class="dropdown-item" href="{{ route('logs') }}">System Error Logs</a>
   </div>
