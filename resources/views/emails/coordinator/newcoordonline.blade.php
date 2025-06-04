af1d3f4ee8dd575e5be06c8b0ed67e87a21c5ac1
MCMIMI <webmaster@momsclub.org>
2025-06-04 11:53:16 -0400

    Update newcoordonline.blade.php

--- resources/views/emails/coordinator/newcoordonline.blade.php
+++ resources/views/emails/coordinator/newcoordonline.blade.php
@@ -6,10 +6,7 @@ A New Coordinator Application has been submitted for Conference {{ $mailData['co
 <br>
 {!! $mailData['mailTable'] !!}
 <br>
-<p>The New Chapter Application Fee is authorize only and must be retrieved in 30 days or the founder will have to resubmit their application. New Chapter must be approved and moved
-    from PENDING to ACTIVE status before fee can be retrieved.
-</p>
-<br>
+
 <strong>MCL,</strong><br>
 MIMI Database Administrator
 @endcomponent
