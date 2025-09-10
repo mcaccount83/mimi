6694ad809e910687b8c83ed10416236fcb5eb834
Shift <shift@laravelshift.com>
2025-09-10 17:38:58 +0000

    Leverage injected request object
    Laravel automatically injects the current Http [request object][1] to all Controller actions and Middleware. Leveraging this object improves consistency and testability.
    
    [1]: https://laravel.com/docs/requests#accessing-the-request

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -899,7 +899,7 @@ class PDFController extends Controller
 
         $title = 'IRS Subordinate Filing';
         $message = "Full subordinate list of updates, additions and deletions from $startFormatted - $todayFormatted.<br><br>Some additions or deletions may have been included in a previous submission, but are included here to ensure a full and complete report.";
-        $pages = request()->query('pages') ?? request()->input('pages') ?? 1;
+        $pages = $request->query('pages') ?? $request->input('pages') ?? 1;
 
         // 1. Generate both DOMPDFs
         $cover = $this->generateEODeptFaxCover(false, $title, $message, $pages);
@@ -950,7 +950,7 @@ class PDFController extends Controller
         $user = $this->userController->loadUserInformation($request);
         $coorId = $user['user_coorId'];
 
-        $dateInput = request()->query('date') ?? request()->input('date');
+        $dateInput = $request->query('date') ?? $request->input('date');
         $date = $dateInput ? Carbon::parse($dateInput) : Carbon::now();
         $startFormatted = $date->format('F Y');
         $todayDate = Carbon::now();
@@ -1014,7 +1014,7 @@ class PDFController extends Controller
 
         $title = 'IRS Updates';
         $message = 'Subordinate corrections. Includes any additions and deletions as follows.';
-        $pages = request()->query('pages') ?? request()->input('pages') ?? 1;
+        $pages = $request->query('pages') ?? $request->input('pages') ?? 1;
 
         // 1. Generate both DOMPDFs
         $cover = $this->generateEODeptFaxCover(false, $title, $message, $pages);
@@ -1065,10 +1065,10 @@ class PDFController extends Controller
         $user = $this->userController->loadUserInformation($request);
         $coorId = $user['user_coorId'];
 
-        $pages = request()->query('pages') ?? request()->input('pages') ?? 1;
+        $pages = $request->query('pages') ?? $request->input('pages') ?? 1;
         $totalPages = (int) $pages;
         $followPages = $totalPages - 1;
-        $dateInput = request()->query('date') ?? request()->input('date');
+        $dateInput = $request->query('date') ?? $request->input('date');
         $date = $dateInput ? Carbon::parse($dateInput) : Carbon::now();
         $startFormatted = $date->format('m-d-Y');
 
@@ -1246,7 +1246,7 @@ class PDFController extends Controller
 
         $title = 'IRS Updates';
         $message = 'Subordinate corrections. Chapters could not file 990N.';
-        $pages = request()->query('pages') ?? request()->input('pages') ?? 1;
+        $pages = $request->query('pages') ?? $request->input('pages') ?? 1;
 
         // 1. Generate both DOMPDFs
         $cover = $this->generateEODeptFaxCover(false, $title, $message, $pages);
@@ -1297,7 +1297,7 @@ class PDFController extends Controller
         $user = $this->userController->loadUserInformation($request);
         $coorId = $user['user_coorId'];
 
-        $pages = request()->query('pages') ?? request()->input('pages') ?? 1;
+        $pages = $request->query('pages') ?? $request->input('pages') ?? 1;
         $totalPages = (int) $pages;
         $followPages = $totalPages - 1;
 
@@ -1403,11 +1403,11 @@ class PDFController extends Controller
     // /**
     //  * EO Dept IRS Fax Coversheet
     //  */
-    public function generateEODeptFaxCover($streamResponse = true, $title = null, $message = null, $pages = null)
+    public function generateEODeptFaxCover(Request $request, $streamResponse = true, $title = null, $message = null, $pages = null)
     {
-        $pages = $pages ?? request()->query('pages') ?? request()->input('pages') ?? 1;
-        $message = $message ?? request()->query('message') ?? request()->input('message') ?? '';
-        $title = $title ?? request()->query('title') ?? request()->input('title') ?? 'Fax Cover Sheet';
+        $pages = $pages ?? $request->query('pages') ?? $request->input('pages') ?? 1;
+        $message = $message ?? $request->query('message') ?? $request->input('message') ?? '';
+        $title = $title ?? $request->query('title') ?? $request->input('title') ?? 'Fax Cover Sheet';
 
         $totalPages = (int) $pages;
         $followPages = $totalPages - 1;
