f986227eb856f83154ddeb7a5c9fad9fbf7d2df1
Shift <shift@laravelshift.com>
2025-09-10 17:38:56 +0000

    Convert controller validation to Form Requests

---
+++ app/Http/Requests/UpdateConfListAdminRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateConfListAdminRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+'id'                     => 'required|exists:conference,id',
+'conference_name'        => 'required|string|max:255',
+'short_name'             => 'required|string|max:50',
+'conference_description' => 'required|string|max:500',
+'short_description'      => 'required|string|max:10',
+];
+    }
+}
