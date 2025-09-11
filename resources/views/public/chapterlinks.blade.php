fdb6f7bc1b0d16a12a809bf2329438e6d48a947a
MCMIMI <webmaster@momsclub.org>
2025-09-11 14:27:00 -0400

    Update chapterlinks.blade.php

--- resources/views/public/chapterlinks.blade.php
+++ resources/views/public/chapterlinks.blade.php
@@ -93,27 +93,15 @@
                             <div id="collapse{{ $loop->index + count($international) }}" class="collapse" data-parent="#usaAccordion">
                                 <div class="card-body">
                 @endif
-{{--
-                                   <div class="chapter">
-                                        <a href="/chapters-2/chapterinfo?chapter_id={{ $chapter->id }}" target="_blank">
-                                            {{ $chapter->name }}
-                                        </a>
-                                        </div> --}}
-
-                                        <div class="chapter">
-                                            <a href="#" class="chapter-link" data-id="{{ $chapter->id }}">
-                                                {{ $chapter->name }}
-                                            </a>
-                                        </div>
 
 
-                                    {{-- <div class="chapter">
+                                    <div class="chapter">
                                         @if($chapter->website_status == 1)
                                             <a href="{{ $chapter->website_url }}" target="_blank">{{ $chapter->name }}</a>
                                         @else
                                             <a href="https://momsclub.org/chapters/find-a-chapter/" target="_blank">{{ $chapter->name }}</a>
                                         @endif
-                                    </div> --}}
+                                    </div>
                 @php
                     $previousState = $chapter->state_long_name;
                 @endphp
