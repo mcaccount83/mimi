04c7ffb7a89c6a83850d7aac14e325228db701fc
MCMIMI <webmaster@momsclub.org>
2025-09-15 10:37:43 -0400

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -34,4 +34,6 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })
+    ->create()
+    ->useTimezone('America/New_York'); // Add this line
