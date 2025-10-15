ddba0989e2313de65603d6a1ae5c922526c1091d
MCMIMI <webmaster@momsclub.org>
2025-10-15 15:55:55 -0400

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,34 +23,26 @@ class AppServiceProvider extends ServiceProvider
     public const HOME = '/home';
 
     /**
-     * Bootstrap any application services.
-     */
-   public function boot(): void
-{
-    Schema::defaultStringLength(191);
+    * Bootstrap any application services.
+    */
+    public function boot(): void
+    {
+        Schema::defaultStringLength(191);
 
-    // Register mail markdown views namespace
-    $this->loadViewsFrom(resource_path('views/vendor/mail/html'), 'mail');
+        // Register mail markdown views namespace
+        $this->loadViewsFrom(resource_path('views/vendor/mail/html'), 'mail');
 
-    // Add global BCC to all emails
-    Event::listen(MessageSending::class, function ($event) {
-        $bccAddress = config('mail.bcc.address');
+        // Add global BCC to all emails
+        Event::listen(MessageSending::class, function ($event) {
+            $bccAddress = config('mail.bcc.address');
 
-        if ($bccAddress) {
-            $event->message->bcc($bccAddress);
+            if ($bccAddress) {
+                $event->message->bcc($bccAddress);
+            }
+        });
 
-            // Get all BCC addresses to verify it was added
-            $bccList = $event->message->getBcc();
-            Log::info('BCC added successfully', [
-                'config_bcc' => $bccAddress,
-                'message_bcc_list' => $bccList ? array_keys($bccList) : 'EMPTY',
-                'subject' => $event->message->getSubject()
-            ]);
-        }
-    });
-
-    $this->bootRoute();
-}
+        $this->bootRoute();
+    }
 
     /**
      * Register any application services.
