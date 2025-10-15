2cf25c88fa718c00c53a2498b97a6bc6a7b56108
Shift <shift@laravelshift.com>
2025-10-15 17:16:17 +0000

    Default new `bootstrap/app.php`

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,15 +1,8 @@
 <?php
 
-use App\Http\Middleware\CoordinatorListAccessCMiddleware;
-use App\Http\Middleware\CoordinatorListAccessPMiddleware;
-use App\Http\Middleware\CoordinatorListAccessTMiddleware;
-use App\Http\Middleware\HandlePageExpired;
-use App\Providers\AppServiceProvider;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Log;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withProviders()
@@ -21,64 +14,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->redirectGuestsTo(fn () => route('login'));
-        $middleware->redirectUsersTo(AppServiceProvider::HOME);
-        $middleware->append(HandlePageExpired::class);
-        $middleware->throttleApi();
-        $middleware->alias([
-            'coordinatorlistC.access' => CoordinatorListAccessCMiddleware::class,
-            'coordinatorlistT.access' => CoordinatorListAccessTMiddleware::class,
-            'coordinatorlistP.access' => CoordinatorListAccessPMiddleware::class,
-            'admin' => \App\Http\Middleware\AdminMiddleware::class,
-        ]);
+        //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        $exceptions->reportable(function (Throwable $e) {
-            $context = [
-                'url' => request()->fullUrl(),
-                'method' => request()->method(),
-                'user' => Auth::id() ?? 'guest',
-                'input' => collect(request()->all())->except(['password'])->toArray(),
-                'referrer' => request()->headers->get('referer'),
-                'trace' => $e->getTraceAsString(),
-            ];
-
-            Log::error($e->getMessage(), $context);
-        });
+        //
     })->create();
-
-// use App\Http\Middleware\CoordinatorListAccessCMiddleware;
-// use App\Http\Middleware\CoordinatorListAccessPMiddleware;
-// use App\Http\Middleware\CoordinatorListAccessTMiddleware;
-// use App\Http\Middleware\HandlePageExpired;
-// use App\Providers\AppServiceProvider;
-// use Illuminate\Foundation\Application;
-// use Illuminate\Foundation\Configuration\Exceptions;
-// use Illuminate\Foundation\Configuration\Middleware;
-
-// return Application::configure(basePath: dirname(__DIR__))
-//     ->withProviders([
-//         \Barryvdh\DomPDF\ServiceProvider::class,
-//     ])
-//     ->withRouting(
-//         web: __DIR__.'/../routes/web.php',
-//         commands: __DIR__.'/../routes/console.php',
-//         health: '/up',
-//     )
-//     ->withMiddleware(function (Middleware $middleware) {
-//         $middleware->redirectGuestsTo(fn () => route('login'));
-//         $middleware->redirectUsersTo(AppServiceProvider::HOME);
-//         $middleware->append(HandlePageExpired::class);
-//         $middleware->throttleApi();
-//         $middleware->alias([
-//             'coordinatorlistC.access' => CoordinatorListAccessCMiddleware::class,
-//             'coordinatorlistT.access' => CoordinatorListAccessTMiddleware::class,
-//             'coordinatorlistP.access' => CoordinatorListAccessPMiddleware::class,
-//             'admin' => \App\Http\Middleware\AdminMiddleware::class,
-
-//         ]);
-
-//     })
-//     ->withExceptions(function (Exceptions $exceptions) {
-//         //
-//     })->create();
