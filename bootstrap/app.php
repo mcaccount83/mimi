7151cb477bf10bae3b50c1b427b67933e0933e41
Shift <shift@laravelshift.com>
2025-10-15 12:21:40 +0000

    Default new `bootstrap/app.php`

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,36 +1,20 @@
 <?php
 
-use App\Http\Middleware\CoordinatorListAccessCMiddleware;
-use App\Http\Middleware\CoordinatorListAccessPMiddleware;
-use App\Http\Middleware\CoordinatorListAccessTMiddleware;
-use App\Http\Middleware\HandlePageExpired;
-use App\Providers\AppServiceProvider;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withProviders([
-        \Barryvdh\DomPDF\ServiceProvider::class,
-    ])
+    ->withProviders()
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        // api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
+        // channels: __DIR__.'/../routes/channels.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->redirectGuestsTo(fn () => route('login'));
-        $middleware->redirectUsersTo(AppServiceProvider::HOME);
-        $middleware->append(HandlePageExpired::class);
-        $middleware->throttleApi();
-        $middleware->alias([
-            'coordinatorlistC.access' => CoordinatorListAccessCMiddleware::class,
-            'coordinatorlistT.access' => CoordinatorListAccessTMiddleware::class,
-            'coordinatorlistP.access' => CoordinatorListAccessPMiddleware::class,
-            'admin' => \App\Http\Middleware\AdminMiddleware::class,
-
-        ]);
-
+        //
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
