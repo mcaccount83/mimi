92b419f3c363b0538dc44cb07ae791665fc3ad17
MCMIMI <webmaster@momsclub.org>
2025-10-16 08:25:10 -0400

    laravel fixer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -44,8 +44,8 @@ class AppServiceProvider extends ServiceProvider
     }
 
     /**
-     * Register any application services.
-     */
+    * Register any application services.
+    */
     public function register(): void
     {
         //
