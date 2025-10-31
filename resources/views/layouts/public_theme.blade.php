e34f1f02050558906da827fea3da17c0693f8f1a
MCMIMI <webmaster@momsclub.org>
2025-10-30 20:49:52 -0400

    messages

--- resources/views/layouts/public_theme.blade.php
+++ resources/views/layouts/public_theme.blade.php
@@ -59,7 +59,77 @@
 
     <!-- Main content -->
     <div class="content">
-        @include('layouts.scripts.messages')
+        @if ($message = Session::get('success'))
+        <script>
+            document.addEventListener("DOMContentLoaded", function() {
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: @json($message),
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            });
+        </script>
+    @endif
+
+    @if ($message = Session::get('info'))
+        <script>
+            document.addEventListener("DOMContentLoaded", function() {
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'info',
+                    title: @json($message),
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            });
+        </script>
+    @endif
+
+    @if ($message = Session::get('warning'))
+        <script>
+            document.addEventListener("DOMContentLoaded", function() {
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'warning',
+                    title: @json($message),
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            });
+        </script>
+    @endif
+
+    @if ($message = Session::get('fail'))
+        <script>
+            document.addEventListener("DOMContentLoaded", function() {
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'error',
+                    title: @json($message),
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            });
+        </script>
+    @endif
+
+        @if ($errors->any())
+            <script>
+                document.addEventListener("DOMContentLoaded", function() {
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'error',
+                        title: 'There were some errors!',
+                        html: '<ul>@foreach ($errors->all() as $error)<li>{{ $error }}</li>@endforeach</ul>',
+                        showConfirmButton: true,
+                    });
+                });
+            </script>
+        @endif
+
+
 
         @yield('content')
     </div>
