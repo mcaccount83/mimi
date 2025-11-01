3017af20f925d565844f4980066fb917491f0b20
MCMIMI <webmaster@momsclub.org>
2025-11-01 12:28:34 -0400

    laravel fixer

--- app/Enums/CoordinatorCheckbox.php
+++ app/Enums/CoordinatorCheckbox.php
@@ -6,19 +6,28 @@ class CoordinatorCheckbox
 {
     // Coordinator List Checkboxes
     const DIRECT_REPORT = 'check';      // Only show coordinators I am directly supervisor for
-    // const REVIEWER = 'check2';
-    const CONFERENCE_REGION = 'check3';       // Show all coordinators in conference/region (based on position)
-    // const PROBATION = 'check4';
-    const INTERNATIONAL = 'check5';           // Show all international coordinators
-        const REPORTING_TREE = 'check6';          // Show Mary's full reporting tree
 
+    // const REVIEWER = 'check2';
+
+    const CONFERENCE_REGION = 'check3';       // Show all coordinators in conference/region (based on position)
+
+    // const PROBATION = 'check4';
+
+    const INTERNATIONAL = 'check5';           // Show all international coordinators
+
+    const REPORTING_TREE = 'check6';          // Show Mary's full reporting tree
 
     // Checkbox Keys (for return array)
     const CHECK_DIRECT = 'checkBoxStatus';
+
     // const CHECK_REVIEWER = 'checkBox2Status';
+
     const CHECK_CONFERENCE_REGION = 'checkBox3Status';
+
     // const CHECK_PROBATION = 'checkBox4Status';
+
     const CHECK_INTERNATIONAL = 'checkBox5Status';
-        const CHECK_REPORTING_TREE = 'checkBox6Status';
+
+    const CHECK_REPORTING_TREE = 'checkBox6Status';
 
 }
--- app/Enums/CoordinatorPosition.php
+++ app/Enums/CoordinatorPosition.php
@@ -6,34 +6,60 @@ class CoordinatorPosition
 {
     // Coordinator Positions (Hierarchical)
     const BS = 1;
+
     const AC = 2;
+
     const SC = 3;
+
     const ARC = 4;
+
     const RC = 5;
+
     const ACC = 6;
+
     const CC = 7;
+
     const FOUNDER = 8;
 
     // Special Positions
     const WR = 9;
+
     const REG = 10;
+
     const CDC = 11;
+
     const EIN = 12;
+
     const IT = 13;
+
     const SMC = 14;
+
     const IC = 15;
+
     const CNE = 16;
+
     const COR = 17;
+
     const IIC = 18;
+
     const ARR = 19;
+
     const M2M = 20;
+
     const M2M2 = 21;
+
     const SPC = 22;
+
     const LIST = 23;
+
     const BC = 24;
+
     const LOA = 26;
+
     const TEST = 27;
+
     const BSM = 28;
+
     const ART = 29;
 
 }
