3778224f599b0c2d156cd8718395658870ae51f9
MCMIMI <webmaster@momsclub.org>
2025-12-19 11:00:30 -0500

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,9 +6,13 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
+
+    use Notifiable;
+
     protected $primaryKey = 'id';
 
     protected $guarded = [ ]; // ALL columns are mass-assignable
