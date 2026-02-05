eccc00b0d9b5e3cbe29625c0b166f68f2fb5b1e8
MCMIMI <webmaster@momsclub.org>
2026-02-05 16:13:09 -0500

    grant list hidden

--- resources/views/boards/profile.blade.php
+++ resources/views/boards/profile.blade.php
@@ -544,7 +544,7 @@
                         <h5>Resources</h5>
                             <button id="Resources" type="button" class="btn bg-primary mb-1 btn-sm" onclick="window.location='{{ route('board.viewresources', ['id' => $chDetails->id]) }}'">Chapter Resources</button><br>
                             <button id="Resources" type="button" class="btn bg-primary mb-1 btn-sm" onclick="window.location='{{ route('board.viewelearning', ['id' => $chDetails->id]) }}'">eLearning Library</button><br>
-                            <button id="Resources" type="button" class="btn bg-primary mb-1 btn-sm" onclick="window.location='{{ route('board.viewgrantrequestlist', ['id' => $chDetails->id]) }}'">Chapter Grants List</button><br>
+                            {{-- <button id="Resources" type="button" class="btn bg-primary mb-1 btn-sm" onclick="window.location='{{ route('board.viewgrantrequestlist', ['id' => $chDetails->id]) }}'">Chapter Grants List</button><br> --}}
                             {{-- <button id="eLearning" type="button"  onclick="window.open('https://momsclub.org/elearning/')" class="btn bg-primary mb-1 btn-sm">eLearning Library</button><br> --}}
 
                         </li>
