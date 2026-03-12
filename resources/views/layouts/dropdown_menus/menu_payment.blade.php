a5cfcf14b493a85270db268ca1e7fdc515e40076
MCMIMI <webmaster@momsclub.org>
2026-03-12 09:42:44 -0400

    menu

--- resources/views/layouts/dropdown_menus/menu_payment.blade.php
+++ resources/views/layouts/dropdown_menus/menu_payment.blade.php
@@ -1,11 +1,13 @@
 <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-    @if ($coordinatorCondition && $conferenceCoordinatorCondition)
+    @if ($coordinatorCondition || ($coordinatorCondition && $conferenceCoordinatorCondition))
         <a class="dropdown-item" href="{{ route('payment.chapreregistration') }}">Re-Registration Payments</a>
         <a class="dropdown-item" href="{{ route('payment.chapdonations') }}">M2M & Sustaining Donations</a>
-        <a class="dropdown-item" href="{{ route('payment.grantlist') }}">Grant Requests</a>
-    @elseif ($coordinatorCondition)
+        @if ($conferenceCoordinatorCondition)
+            <a class="dropdown-item" href="{{ route('payment.grantlist') }}">Grant Requests</a>
+        @endif
+    {{-- @elseif ($coordinatorCondition)
         <a class="dropdown-item" href="{{ route('payment.chapreregistration') }}">Re-Registration Payments</a>
-        <a class="dropdown-item" href="{{ route('payment.chapdonations') }}">M2M & Sustaining Donations</a>
+        <a class="dropdown-item" href="{{ route('payment.chapdonations') }}">M2M & Sustaining Donations</a> --}}
     @elseif ($m2mCondition || $ITCondition)
         <a class="dropdown-item" href="{{ route('payment.chapreregistration', ['check5' => 'yes']) }}">International Re-Registration Payments</a>
         <a class="dropdown-item" href="{{ route('payment.chapdonations', ['check5' => 'yes']) }}">International M2M & Sustaining Donations</a>
--- resources/views/layouts/dropdown_menus/menu_resources.blade.php
+++ resources/views/layouts/dropdown_menus/menu_resources.blade.php
@@ -2,5 +2,4 @@
     <a class="dropdown-item" href="{{ route('resources.toolkit') }}">Coordinator Toolkit</a>
     <a class="dropdown-item" href="{{ route('resources.resources') }}">Chapter Resources</a>
     <a class="dropdown-item" href="{{ route('resources.elearning') }}">eLearning Library</a>
-    <a class="dropdown-item" href="{{ url(config('forum.frontend.router.prefix') . '/c/2-coordinatorlist') }}" target="_blank">CoordinatorList Forum</a>
 </div>
