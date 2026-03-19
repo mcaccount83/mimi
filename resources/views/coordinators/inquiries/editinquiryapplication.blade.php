ecb057216255189c00c928adf827415ed36b794c
MCMIMI <webmaster@momsclub.org>
2026-03-19 14:44:50 -0400

    Update editinquiryapplication.blade.php

--- resources/views/coordinators/inquiries/editinquiryapplication.blade.php
+++ resources/views/coordinators/inquiries/editinquiryapplication.blade.php
@@ -33,15 +33,19 @@
             <div class="card card-primary card-outline">
                 <div class="card-body">
                     <div class="card-header text-center bg-transparent">
-                        <h3><b>{{ $inqDetails->state->state_long_name}}</b></h3>
+                        <h3 class="mb-0">{{ $inqDetails->state->state_long_name}}</h3>
+                        <p class="mb-0">{{ $inqDetails->state->conference->conference_description }} Conference, {{ $inqDetails->state->region->long_name }} Region
+                        <br>
+
+                        {{-- <h3><b>{{ $inqDetails->state->state_long_name}}</b></h3>
                         <h3>{{ $inqDetails->state->region->long_name }} Region</h3>
-                        <h3>{{ $inqDetails->state->conference->conference_description }} Conference</h3>
+                        <h3>{{ $inqDetails->state->conference->conference_description }} Conference</h3> --}}
                         @if ($inquiriesMapLink)
-                            <a href="{{ $inquiriesMapLink }}" target="_blank" class="btn btn-primary bg-gradient btn-sm m-1">
+                            <a href="{{ $inquiriesMapLink }}" target="_blank" class="btn btn-primary bg-gradient btn-xs m-1">
                                 <i class="bi bi-pin-map-fill me-2"></i>VIEW REGION MAP
                             </a>
                         @else
-                            <button type="button" class="btn btn-primary bg-gradient btn-sm m-1" disabled>
+                            <button type="button" class="btn btn-primary bg-gradient btn-xs m-1" disabled>
                                 <i class="bi bi-pin-map-fill me-2"></i>VIEW REGION MAP
                             </button>
                         @endif
