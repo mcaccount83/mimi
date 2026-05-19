c4687ffd57bce29ec7dcef551872d59708552ed6
Shift <shift@laravelshift.com>
2026-05-19 23:50:06 +0000

    Convert validation rules to array syntax

--- app/Http/Requests/UpdateAdminEmailTechReportRequest.php
+++ app/Http/Requests/UpdateAdminEmailTechReportRequest.php
@@ -14,9 +14,21 @@ class UpdateAdminEmailTechReportRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required|string|max:255',
-            'description' => 'required|string|max:255',
-            'email' => 'required|string|max:255',
+            'name' => [
+                'required',
+                'string',
+                'max:255',
+            ],
+            'description' => [
+                'required',
+                'string',
+                'max:255',
+            ],
+            'email' => [
+                'required',
+                'string',
+                'max:255',
+            ],
         ];
     }
 }
