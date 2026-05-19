4e68b440ca07cae88add64096ed17b47542d352b
Shift <shift@laravelshift.com>
2026-05-19 23:50:28 +0000

    Remove redundant typing from DocBlocks

--- app/Http/Requests/AddAdminEmailTechReportRequest.php
+++ app/Http/Requests/AddAdminEmailTechReportRequest.php
@@ -8,8 +8,6 @@ class AddAdminEmailTechReportRequest extends FormRequest
 {
     /**
      * Get the validation rules that apply to the request.
-     *
-     * @return array
      */
     public function rules(): array
     {
