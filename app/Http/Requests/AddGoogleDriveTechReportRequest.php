e55b0c38466a9bbc0d487597bcc039a659b134a5
Shift <shift@laravelshift.com>
2026-05-21 12:40:01 +0000

    Remove redundant typing from DocBlocks

--- app/Http/Requests/AddGoogleDriveTechReportRequest.php
+++ app/Http/Requests/AddGoogleDriveTechReportRequest.php
@@ -8,8 +8,6 @@ class AddGoogleDriveTechReportRequest extends FormRequest
 {
     /**
      * Get the validation rules that apply to the request.
-     *
-     * @return array
      */
     public function rules(): array
     {
