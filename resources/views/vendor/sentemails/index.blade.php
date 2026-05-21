d319446d24e408fbecc1730960b6a5df4eb6047a
MCMIMI <webmaster@momsclub.org>
2026-05-21 11:34:05 -0400

    laravel shift

--- resources/views/vendor/sentemails/index.blade.php
+++ resources/views/vendor/sentemails/index.blade.php
@@ -6,20 +6,17 @@
 <script src="https://cdn.tailwindcss.com"></script>
 
 @section('content')
-{{-- @if(session('error')) --}}
 @session('error')
 <script>
     document.addEventListener('DOMContentLoaded', function() {
         Swal.fire({
             icon: 'error',
             title: 'Attachment Not Found',
-            // text: '{{ session('error') }}',
             text: '{{ $value }}',
         });
     });
 </script>
 @endsession
-{{-- @endif --}}
     <!-- Main content -->
     <section class="content">
         <div class="container-fluid table-container">
