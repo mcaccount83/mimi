3e5bb5e4aebdb3ae6b3bdbdd0a4362640ec3b9f5
MCMIMI <webmaster@momsclub.org>
2026-06-08 17:32:43 -0400

    email logo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,6 +1,12 @@
 <tr>
 <td class="header">
-<a href="{{ $url }}">
-International MOMS Club</a>
+    <a href="{{ $url }}" style="display:inline-block;">
+        <img src="{{ config('settings.base_url') }}images/logo-moms.png" alt="International MOMS Club" width="120">
+    {{-- <img src="https://momsclub.org/images/logo.png"
+         alt="International MOMS Club"
+         style="max-height:60px; width:auto; border:0;" /> --}}
+</a>
+{{-- <a href="{{ $url }}">
+International MOMS Club</a> --}}
 </td>
 </tr>
