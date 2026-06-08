142e1131eb281c5a577e016b85a3eec7136a6e1a
MCMIMI <webmaster@momsclub.org>
2026-06-08 18:07:29 -0400

    logo-long

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,7 @@
     <a href="{{ $url }}" style="display:inline-block;">
         {{-- <img src="{{ config('app.url') }}/public/images/logo-moms.png" alt="International MOMS Club" style="width: 125px;"> --}}
 
-        <img src="{{ config('settings.base_url') }}images/logo-moms.png" alt="International MOMS Club" width="120">
+        <img src="{{ config('settings.base_url') }}images/logo-long.png" alt="International MOMS Club" style="width: 250px;">
     {{-- <img src="https://momsclub.org/images/logo.png"
          alt="International MOMS Club"
          style="max-height:60px; width:auto; border:0;" /> --}}
