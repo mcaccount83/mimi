8314344e2c9ebcc99f8ba12cdca6e8e475cf2d47
MCMIMI <webmaster@momsclub.org>
2026-06-09 12:37:01 -0400

    Update header.blade.php

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,9 +1,7 @@
 <tr>
 <td class="header">
     <a href="{{ $url }}" style="display:inline-block;">
-        {{-- <img src="{{ $_SERVER['DOCUMENT_ROOT'] . '/' . ltrim(config('settings.base_url'), '/') . 'images/logo-long.png' }}" alt="International MOMS Club" style="width: 250px;"> --}}
         <img src="{{ config('settings.base_url') }}images/logo-long.png" alt="International MOMS Club" width="250">
-        {{-- <img src="{{ config('settings.mail_image_base_url') }}logo-long.png" alt="International MOMS Club" width="250"> --}}
     </a>
 {{-- <a href="{{ $url }}">
 International MOMS Club</a> --}}
