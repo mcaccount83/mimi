45a02e3155b45c79baa8421a773ed3e7022e27f5
MCMIMI <webmaster@momsclub.org>
2026-06-30 20:34:27 -0400

    Update app.php

--- config/app.php
+++ config/app.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Facade;
 
 return [
 
+    'timezone' => env('APP_TIMEZONE', 'America/New_York'),
+
     'aliases' => Facade::defaultAliases()->merge([
         'PDF' => Barryvdh\DomPDF\Facade\Pdf::class,
     ])->toArray(),
