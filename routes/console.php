dab761b4b795d1cae9d47893b03f0056dfee18ba
MCMIMI <webmaster@momsclub.org>
2026-07-06 15:30:38 -0400

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -2,5 +2,8 @@
 
 use Illuminate\Support\Facades\Schedule;
 
-// Run the queue worker every minute
-Schedule::command('queue:work --stop-when-empty')->everyMinute();
+Schedule::command('queue:work --stop-when-empty --tries=3 --timeout=50 --sleep=2 --max-jobs=10')->everyMinute();
+
+Schedule::call(function () {
+    \Illuminate\Support\Facades\Log::info('Scheduler heartbeat: ' . now());
+})->everyMinute();
