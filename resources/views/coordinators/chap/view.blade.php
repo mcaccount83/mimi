0ed6e4d89e932a8262b5f3cd9e3a1247e35b022a
MCMIMI <webmaster@momsclub.org>
2026-07-08 15:41:00 -0400

    Update view.blade.php

--- resources/views/coordinators/chap/view.blade.php
+++ resources/views/coordinators/chap/view.blade.php
@@ -530,24 +530,29 @@
                                     @endif
                             </div> --}}
 
-@php
-    $chapter_awards_check = [];
-    if (!empty($chFinancialReport['chapter_awards'])) {
-        $decoded = unserialize(base64_decode($chFinancialReport['chapter_awards']));
-        if (is_array($decoded)) {
-            $chapter_awards_check = array_filter($decoded, fn ($a) => !empty($a['awards_type']));
+<div class="row mb-2">
+    <div class="col-sm-3">
+        <label>Chapter Awards:</label>
+    </div>
+    @php
+        $chapter_awards_check = [];
+        if (!empty($chFinancialReport['chapter_awards'])) {
+            $decoded = unserialize(base64_decode($chFinancialReport['chapter_awards']));
+            if (is_array($decoded)) {
+                $chapter_awards_check = array_filter($decoded, fn ($a) => !empty($a['awards_type']));
+            }
         }
-    }
-@endphp
-@if (count($chapter_awards_check) > 0)
-    <div class="col-sm-9">
-        Chapter was nominated for one or more awards.
-    </div>
-@else
-    <div class="col-sm-9">
-        Chapter was not nominated for any awards.
-    </div>
-@endif
+    @endphp
+    @if (count($chapter_awards_check) > 0)
+        <div class="col-sm-9">
+            Chapter was nominated for one or more awards.
+        </div>
+    @else
+        <div class="col-sm-9">
+            Chapter was not nominated for any awards.
+        </div>
+    @endif
+</div>
 
                         @else
                             <strong>Report Status/Links are not available at this time.</strong>
